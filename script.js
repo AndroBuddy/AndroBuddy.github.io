233d85e0f58ddaf7c928bcd655d7f3e7f185bf52
AndroBuddy <anirudhchimpidi8@gmail.com>
2020-02-25 20:13:53 +0530

    Add Dark Mode

---
+++ script.js
@@ -0,0 +1,16 @@
+$( ".darky" ).on("click", function() {
+    if( $( "body" ).hasClass( "dark" )) {
+      $( "body" ).removeClass( "dark" );
+    } else {
+      $( "body" ).addClass( "dark" );
+    }
+});
+
+$( ".darkl" ).on("click", function() {
+    if( $( "body" ).hasClass( "dark" )) {
+      $( "body" ).removeClass( "dark" );
+    } else {
+      $( "body" ).addClass( "dark" );
+    }
+});
+
